dcf5c403f639e3e018091fcf489fccbd0479abd0
DevPBDias <devpbdias@gmail.com>
2023-02-13 11:36:35 -0300

    feat: adding new order in class TrackOrders

--- src/track_orders.py
+++ src/track_orders.py
@@ -5,14 +5,16 @@ class TrackOrders:
     # Dia mais movimentado;
     # Dia menos movimentado.
     def __init__(self):
-        self.orders = []
+        self.orders_meals = []
 
     # aqui deve expor a quantidade de estoque
     def __len__(self):
-        return len(self.orders)
+        return len(self.orders_meals)
 
+    # registra um pedido na instancia
     def add_new_order(self, customer, order, day):
-        pass
+        dict_order = {"customer": customer, "order": order, "day": day}
+        self.orders_meals.append(dict_order)
 
     def get_most_ordered_dish_per_customer(self, customer):
         pass
